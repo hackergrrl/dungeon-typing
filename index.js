aedbbf93d18d0ddebb44a162e2acd60fdac0d2b8
noffle <sww@eight45.net>
2018-05-05 14:11:58 -0700

    unused code

--- index.js
+++ index.js
@@ -56,17 +56,6 @@ var projection
 var world
 var map
 
-function vecify (v) {
-  return vec3.fromValues(v.x, v.y, v.z)
-}
-
-function pointLight (lpos, lightIntensity, vpos, normal) {
-  var out = vec3.create()
-  var dir = vec3.sub(out, vpos, lpos)
-  var dist = vec3.length(out)
-  return Math.min(2.0, Math.max(0, lightIntensity / (dist*dist)))
-}
-
 function canSee (a, b) {
   var x = a.physics.pos.x
   var z = a.physics.pos.z
